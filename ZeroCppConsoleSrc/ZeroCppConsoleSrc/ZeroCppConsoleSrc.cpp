a584739d9007083684fd3d51986d1138c1c30281
ZeroX96 <Mahmoud.S.AbdElhares@gmail.com>
2022-11-26 00:16:34 +0200

    Commit-0x0003: Completed and Tested Chapter 02 --------------   -Tested and Finalized CH02

--- ZeroCppConsoleSrc/ZeroCppConsoleSrc/ZeroCppConsoleSrc.cpp
+++ ZeroCppConsoleSrc/ZeroCppConsoleSrc/ZeroCppConsoleSrc.cpp
@@ -12,8 +12,10 @@ int main(void)
 	Ch02 TestCh02;
 
 	//test the class data and functions
-	TestCh02.FirstProgramInCpp_PrintingLineOfText();
 	TestCh02.AnotherCppProgramAddingIntegers();
+	TestCh02.ArithmeticOperations();
+	TestCh02.DecisionMakingEqualityAndRelationalOperators();
+	TestCh02.FirstProgramInCpp_PrintingLineOfText();
 	TestCh02.ModifyingOurFirstCppProgram();
 
 	return 0;
